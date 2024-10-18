e6fc72fef07c2a3d9840cb711bc44836b6eeb5d4
Veta Lantukh <147886896+vetta-unicorn@users.noreply.github.com>
2024-10-18 15:23:34 +0700

    Add files via upload

---
+++ Prac_2_part_1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Arrays
+{
+    class making_arrays
+    {
+        private int size;
+
+        public making_arrays(int N)
+        {
+            size = N;
+        }
+
+        public void print_array()
+        {
+            int[] arr_1 = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                arr_1[i] = i + 1;
+            }
+
+            for (int i = 1; i < size + 1; i++)
+            {
+                Console.WriteLine(arr_1[^i]);
+            }
+        }
+    }
+
+    class making_matrix_with_zeroes
+    {
+        private int line, column;
+
+        public making_matrix_with_zeroes(int N, int M)
+        {
+            line = N;
+            column = M;
+        }
+
+        public void completing_matrix(int N, int M)
+        {
+            int[,] arr_m = new int[line, column];
+
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    if (i <= j + 1) arr_m[i, j] = 1;
+                    else arr_m[i, j] = 0;
+                }
+            }
+
+            string[] rows = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rows[i] += arr_m[i, j];
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(rows[i]);
+            }
+        }
+    }
+
+    class making_round_matrix
+    {
+        private int line, column;
+
+        public making_round_matrix(int N, int M)
+        {
+            line = N; column = M;
+        }
+
+        public void completing_round_matrix(int N, int M)
+        {
+            int[,] arr_r = new int[line, column];
+
+            int Flag = 0; // индикатор направления
+
+            int counter_lines = 0;
+            int counter_columns = 0;
+
+            int counter_row = 0;
+
+            for (int i = 1; i < line * column + 1;i ++)
+            {
+                int counter_for_flag0 = 1;
+
+                switch (Flag)
+                {
+                    case 0:
+                        if (arr_r[counter_lines, counter_columns] != 0)
+                        {
+                            counter_lines = counter_lines + 1;
+                            counter_columns = counter_columns + 1;
+                        }
+
+                        arr_r[counter_lines, counter_columns] = i;
+                        counter_columns++;
+                        if (counter_columns == column - counter_row - 1)
+                        {
+                            Flag = 1;
+                        }
+                        break;
+
+                    case 1:
+                        if (arr_r[counter_lines, counter_columns] != 0)
+                        {
+                            counter_lines = counter_lines - 1;
+                            counter_columns = counter_columns - 1;
+                        }
+
+                        arr_r[counter_lines, counter_columns] = i;
+                        counter_lines++;
+                        if (counter_lines == line - counter_row - 1)
+                        {
+                            Flag = 2;
+                        }
+                        break;
+
+                    case 2:
+                        if (arr_r[counter_lines, counter_columns] != 0)
+                        {
+                            counter_lines = counter_lines - 1;
+                            counter_columns = counter_columns - 1;
+                        }
+
+                        arr_r[counter_lines, counter_columns] = i;
+                        counter_columns--;
+                        if (counter_columns == counter_row)
+                        {
+                            Flag = 3;
+                        }
+                        break;
+
+                    case 3:
+                        if (arr_r[counter_lines, counter_columns] != 0)
+                        {
+                            counter_lines = counter_lines + 1;
+                            counter_columns = counter_columns + 1;
+                        }
+                        arr_r[counter_lines, counter_columns] = i;
+                        counter_lines--;
+                        if (counter_lines == counter_row)
+                        {
+                            Flag = 0;
+                            counter_row++;
+                        }
+                        break;
+                }
+            }
+
+            string[] rows = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rows[i] += arr_r[i, j];
+                }
+            }
+
+            for(int i = 0; i < N; i++)
+            {
+                Console.WriteLine(rows[i]);
+            }
+
+        }
+    }
+
+    class Program
+    {
+        const int N = 5;
+        const int M = 4;
+        static void Main(string[] args)
+        {
+            //вывод одномерного массива
+            Console.WriteLine("Making array and printing in reverse order: ");
+            making_arrays arr = new making_arrays(N);
+            arr.print_array();
+
+            //создание квадратного массива с нулями слева
+            Console.WriteLine(" ");
+            Console.WriteLine("Matrix with zeroes under the diagonal: ");
+            making_matrix_with_zeroes matrix = new making_matrix_with_zeroes(N, M);
+            matrix.completing_matrix(N, M);
+
+            // создание квадратного или прямоугольного массива с заполнением "по кругу"
+            Console.WriteLine("");
+            Console.WriteLine("Circle matrix: ");
+            making_round_matrix arr_r = new making_round_matrix(N, M);
+            arr_r.completing_round_matrix(N, M);
+        }
+    }
+}
---
+++ prac_2_part_2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data;
+
+public enum Operation
+{
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Power,
+    SquareRoot
+}
+
+public class Calculator
+{
+    public static double DoOperation(Operation op, double a, double b = 0)
+    {
+        switch (op)
+        {
+            case Operation.Add:
+                return a + b;
+            case Operation.Subtract:
+                return a - b;
+            case Operation.Multiply:
+                return a * b;
+            case Operation.Divide:
+                if (b == 0)
+                    throw new DivideByZeroException("Деление на ноль невозможно.");
+                return a / b;
+            case Operation.Power:
+                return Math.Pow(a, b);
+            case Operation.SquareRoot:
+                if (a < 0)
+                    throw new ArgumentOutOfRangeException("Невозможно вычислить квадратный корень из отрицательного числа.");
+                return Math.Sqrt(a);
+            default:
+                throw new ArgumentException("Неизвестная операция.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите 2 числа, которые хотите сложить: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Сложение: " + Calculator.DoOperation(Operation.Add, a, b));
+
+        Console.WriteLine("Введите 2 числа, которые хотите вычесть: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+        double d = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Вычитание: " + Calculator.DoOperation(Operation.Subtract, c, d));
+
+        Console.WriteLine("Введите 2 числа, которые хотите умножить: ");
+        double e = Convert.ToDouble(Console.ReadLine());
+        double f = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Умножение: " + Calculator.DoOperation(Operation.Multiply, e, f));
+
+        Console.WriteLine("Введите 2 числа, которые хотите разделить");
+        double g = Convert.ToDouble(Console.ReadLine());
+        double h = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Деление: " + Calculator.DoOperation(Operation.Divide, g, h));
+
+        Console.WriteLine("Введите основание и показатель, чтобы вовзвести в степень: ");
+        double i = Convert.ToDouble(Console.ReadLine());
+        double j = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Возведение в степень: " + Calculator.DoOperation(Operation.Power, i, j));
+
+        Console.WriteLine("Введите число, из которого вы хотите извлечь квадратный корень");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Квадратный корень: " + Calculator.DoOperation(Operation.SquareRoot, x));
+    }
+}
+
